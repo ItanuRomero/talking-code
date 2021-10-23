6bbc4e6006093995bc27b8fa789ae7b8d9e38381
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2021-10-22 22:08:29 -0300

    create voice assistant with pyttsx3
    use the system to print and say to interact with the user

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import speech_recognition as sr
+import pyttsx3
 
 
 def clean_code(line_of_code=str):
@@ -6,6 +7,24 @@ def clean_code(line_of_code=str):
         .replace(' abre parênteses', "(") \
         .replace('aspas', "'") \
         .replace('fecha parênteses', ")")
+
+
+def initialize_voice_assistant(voice_enabled):
+    if voice_enabled:
+        engine = pyttsx3.init()
+        voices = engine.getProperty('voices')
+        engine.setProperty('voice', voices[0].id)
+        rate = engine.getProperty('rate')
+        engine.setProperty('rate', rate - 25)
+        return engine
+    return 'Disabled'
+
+
+def say(voice_assistant, text):
+    print(text)
+    if not voice_assistant == 'Disabled':
+        voice_assistant.say(text)
+        voice_assistant.runAndWait()
 
 
 def listen_and_transfer_to_text(recognizer, microphone):
@@ -24,26 +43,31 @@ def write_on_script_file(line, filename='talked-code.py'):
         script_file.write(line + '\n')
 
 
-def start():
-    print('Sistema de escrita por voz iniciado!')
+def start(voice_enabled=True):
+    voice_assistant = initialize_voice_assistant(voice_enabled)
+    say(voice_assistant, 'Sistema de escrita por voz inicializando...')
     recognizer = sr.Recognizer()
     while True:
         with sr.Microphone() as mic:
-            print('Diga algo: ')
+            say(voice_assistant, 'Diga algo: ')
             line = listen_and_transfer_to_text(recognizer, mic)
             if 'Fechar sistema' in line:
-                print('fechar sistema detectado, finalizando...')
+                say(voice_assistant, 'fechar sistema detectado, finalizando...')
                 break
+            say(voice_assistant, 'Verifique na tela se a frase está correta.')
+            print("Você disse: ", line, "?")
             while True:
-                print("Você disse: ", line, "?")
                 response = listen_and_transfer_to_text(recognizer, mic)
-                print('Sua resposta: ', response)
+                say(voice_assistant, f'Sua resposta: {response}')
                 if 'sim' in response or 'confirmo' in response:
                     line_of_code = clean_code(line)
                     write_on_script_file(line_of_code)
+                    say(voice_assistant, 'Linha escrita com sucesso, vamos para outra?')
                     break
                 elif 'Fechar sistema' in response:
                     return 'Sistema fechado.'
+                elif 'não' in response or 'refazer' in response:
+                    break
     return 'Sucesso total!'
 
 
--- talked-code.py
+++ talked-code.py
@@ -1,3 +1,4 @@
 print( ' para pessoal ' )
 print( ' agora eu estou conversando com o computador ' )
 print( ' sucesso total ' )
+print( ' fala pessoal ' )
